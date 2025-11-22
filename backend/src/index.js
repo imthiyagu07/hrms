7239af536149c0d72672927b93bbd72d3566e651
imthiyagu07 <imthiyagugithub@gmail.com>
2025-11-22 13:40:50 +0530

    add cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -11,6 +12,7 @@ import logRoutes from "./routes/log.route.js"
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({origin: process.env.CLIENT_URL, credentials: true}))
 
 app.use("/api/auth", authRoutes);
 app.use("/api/employees", employeeRoutes);
