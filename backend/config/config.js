9bfc0e57be20682b04ee3f675e088387a7905b5a
imthiyagu07 <imthiyagugithub@gmail.com>
2025-11-23 22:21:16 +0530

    small changes

--- backend/config/config.js
+++ backend/config/config.js
@@ -10,5 +10,20 @@ export default {
         port: process.env.DB_PORT,
         dialect: "postgres",
         logging: false
+    },
+    production: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: "postgres",
+        logging: false,
+        dialectOptions: {
+            ssl: {
+            require: true,
+            rejectUnauthorized: false
+            }
+        }
     }
 }
