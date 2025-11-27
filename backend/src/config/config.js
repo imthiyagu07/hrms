2449c0c31d2015009810200851cd312405810239
imthiyagu07 <imthiyagugithub@gmail.com>
2025-11-27 08:02:38 +0530

    chnages in config.js

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -2,28 +2,29 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export default {
-    development: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: "postgres",
-        logging: false
-    },
-    production: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: "postgres",
-        logging: false,
-        dialectOptions: {
-            ssl: {
-            require: true,
-            rejectUnauthorized: false
-            }
-        }
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "postgres",
+    logging: false
+  },
+
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "postgres",
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
     }
-}
+  }
+};
